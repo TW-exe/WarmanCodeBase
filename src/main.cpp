2dee8befd59972a4acb85985e443db93fe30b339
tw-exe <thomas.wolinski2004@gmail.com>
2024-06-05 11:00:26 +0930

    removed libraries and primary classes, adding movement features to main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,81 @@
+//libraries
 #include <Arduino.h>
-#include "Robot.h"
+#include <A4988.h>
 
-Robot myRobot;
+
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+
+#define WHEELRADIUS 40//in mm 
+
+#define DIR 8
+#define STEP 9
+
+#define MS1 10
+#define MS2 11
+#define MS3 12
+
+#define MS4 5
+#define MS5 6
+#define MS6 7
+
+
+A4988 stepperFrontLeft(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);   // Stepper motor at the front left
+A4988 stepperFrontRight(MOTOR_STEPS, DIR, STEP, MS4, MS5, MS6); //
+
+
+
+
+
+
+
+
+
+// Functions 
+void move_forward(float distance, float vel) {
+  int RPM = vel/(40); // velocity is in mm per second
+  stepperFrontLeft.begin(RPM);
+  stepperFrontRight.begin(RPM);
+
+  stepperFrontLeft.enable();
+  stepperFrontRight.enable();
+
+  stepperFrontLeft.setMicrostep(1);
+  stepperFrontRight.setMicrostep(1);
+
+  stepperFrontLeft.move();
+  stepperFrontRight.move();
+
+
+}
+
+
+void move_backward(){
+
+}
+
+
+void rotate_clockwise(){
+
+}
+
+
+void rotate_anticlockwise(){
+
+}
+
+
+
+
+
 
 void setup() {
-    // Setup code
+  // put your setup code here, to run once:
+
+
+
+
 }
 
 void loop() {
-    myRobot.moveForward();
-    delay(1000);
-    myRobot.moveBackward();
-    delay(1000);
-}
+  // put your main code here, to run repeatedly:
